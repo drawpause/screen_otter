eaa7fe909e95d10df63421d78e753189239b2194
drawpause <user@localhost.localdomain>
2016-03-07 22:06:50 +0200

    Comments

--- screen_otter.py
+++ screen_otter.py
@@ -1,9 +1,22 @@
 import sys, re
 from selenium import webdriver
 
+# Get the command line argument
 url = sys.argv[1]
+
+# Define protocols
+http = 'http://'
+https = 'https://'
+
+# If url doesn't have the protocol prefix
+if http or https not in url:
+    url = http + url
+
+# Make the filename nicer and strip special characters
 filename = re.sub(r"[^\w\s]", ' ', url)
-filename = re.sub(r"\s+", '-', filename) + '.png'
+filename = re.sub(r"\s+", '-', filename) + '.jpg'
+
+# Init PhantomJS
 driver = webdriver.PhantomJS('phantomjs')
 driver.set_window_size(1024, 768)
 driver.get(str(url))
